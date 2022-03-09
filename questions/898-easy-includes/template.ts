66d4134313e8b027a075433cd2f85ea794fd3534
Fábio Matias <fabio_mcm2@hotmail.com>
2022-03-09 22:01:32 +0000

    898 - Includes

--- questions/898-easy-includes/template.ts
+++ questions/898-easy-includes/template.ts
@@ -1,1 +1,1 @@
-type Includes<T extends readonly any[], U> = any;
+type Includes<T extends readonly any[], U> = T extends [infer X, ...infer Y] ? (Equal<X, U> extends true ? true : Includes<Y, U>) : false;
